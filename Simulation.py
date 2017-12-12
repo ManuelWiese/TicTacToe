cd3425f026e3766e537e32ef943675864a54e579
Manuel <aclown@protonmail.com>
2017-12-12 20:40:47 +0100

    Split Statistics.py file to Simulation.py and Statistics.py

---
+++ Simulation.py
@@ -0,0 +1,34 @@
+import random
+from Play import Play
+from Game import Game
+import Agent
+from Statistics import Statistics
+
+
+class Simulation:
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+
+    def simulate(self, numberOfGames):
+        assert isinstance(numberOfGames, int)
+        assert numberOfGames >= 0
+
+        play = Play(self.player1, self.player2)
+        statistics = Statistics()
+
+        for i in range(numberOfGames):
+            game = play.playGame(random.choice([1, 2]))
+            gameState = game.getGameState()
+
+            statistics.countGameState(gameState)
+
+        return statistics
+
+if __name__ == "__main__":
+    player1 = Agent.RandomAgent(1)
+    player2 = Agent.DecisionTreeAgent(2)
+
+    simulation = Simulation(player1, player2)
+
+    print(simulation.simulate(10000))
--- Statistics.py
+++ Statistics.py
@@ -1,8 +1,3 @@
-import random
-from Play import Play
-from Game import Game
-import Agent
-
 
 class Statistics:
     def __init__(self):
@@ -19,43 +14,16 @@ class Statistics:
     def tied(self):
         self.__tied += 1
 
+    def countGameState(self, gameState):
+        if gameState.player1Won():
+            self.player1Won()
+        elif gameState.player2Won():
+            self.player2Won()
+        else:
+            self.tied()
+
     def __str__(self):
         output = ("Tied: {}\n".format(self.__tied)
                   + "Player 1 won: {}\n".format(self.__player1Won)
                   + "Player 2 won: {}".format(self.__player2Won))
         return output
-
-
-
-class Simulation:
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
-
-    def simulate(self, numberOfGames):
-        assert isinstance(numberOfGames, int)
-        assert numberOfGames >= 0
-
-        play = Play(self.player1, self.player2)
-        statistics = Statistics()
-
-        for i in range(numberOfGames):
-            game = play.playGame(random.choice([1, 2]))
-            gameState = game.getGameState()
-
-            if gameState.player1Won():
-                statistics.player1Won()
-            elif gameState.player2Won():
-                statistics.player2Won()
-            else:
-                statistics.tied()
-
-        return statistics
-
-if __name__ == "__main__":
-    player1 = Agent.HumanAgent(1)
-    player2 = Agent.DecisionTreeAgent(2)
-
-    simulation = Simulation(player1, player2)
-
-    print(simulation.simulate(10000))
